ad392a8fe3bc434bbce6d373699f6210461a2444
Yogesh Rathod <yogesh952003@gmail.com>
2025-02-13 21:12:19 +0530

    Finish Implementing first 0x0F instructions
    + Add implementation of instructions from 0x00 to 0x0F
    + Start adding comments describing instruction above each instruction

--- src/executor.cpp
+++ src/executor.cpp
@@ -1,5 +1,6 @@
 #include "executor.hpp"
-#include <format>
+#include "common.hpp"
+#include <cstdint>
 
 uint16_t Executor::ToU16(uint8_t msb, uint8_t lsb)
 {
@@ -22,28 +23,37 @@ void Executor::SetCY(CpuState &state, bool value)
   state.AF.low = (state.AF.low & ~(1U << 4U)) | static_cast<uint8_t>(value << 4U);
 }
 
-template<> void Executor::Execute<0>(CpuState &state, Bus &bus)
+// NOP
+template<> void Executor::Execute<0x00>(CpuState &state, Bus &bus)
 {
   state.t_cycles += 4;
 }
-template<> void Executor::Execute<1>(CpuState &state, Bus &bus)
+
+// LD BC, u16
+template<> void Executor::Execute<0x01>(CpuState &state, Bus &bus)
 {
     auto lsb = bus.Read(state.PC.reg++);
     auto msb = bus.Read(state.PC.reg++);
     state.BC.reg = ToU16(msb, lsb);
     state.t_cycles += 12;
 }
-template<> void Executor::Execute<2>(CpuState &state, Bus &bus)
+
+// LD (BC), A
+template<> void Executor::Execute<0x02>(CpuState &state, Bus &bus)
 {
     bus.Write(state.BC.reg, state.AF.high);
     state.t_cycles += 8;
 }
-template<> void Executor::Execute<3>(CpuState &state, Bus &bus)
+
+// INC BC
+template<> void Executor::Execute<0x03>(CpuState &state, Bus &bus)
 {
     ++state.BC.reg;
     state.t_cycles += 8;
 }
-template<> void Executor::Execute<4>(CpuState &state, Bus &bus)
+
+// INC B
+template<> void Executor::Execute<0x04>(CpuState &state, Bus &bus)
 {
     uint16_t res = state.BC.high + 1;
 
@@ -54,23 +64,29 @@ template<> void Executor::Execute<4>(CpuState &state, Bus &bus)
     state.BC.high = (res & 0xFFU);
     state.t_cycles += 4;
 }
-template<> void Executor::Execute<5>(CpuState &state, Bus &bus)
+
+// DEC B
+template<> void Executor::Execute<0x05>(CpuState &state, Bus &bus)
 {
     uint8_t res = state.BC.high - 1;
 
-    SetH(state, (res & (1U << 4U)) != (state.BC.high & (1U << 4U)));
-    SetN(state, true);
     SetZ(state, res == 0);
+    SetN(state, true);
+    SetH(state, ((state.BC.high & 0xFU) - 1) > 0xFU);
 
     state.BC.high = res;
     state.t_cycles += 4;
 }
-template<> void Executor::Execute<6>(CpuState &state, Bus &bus)
+
+// LD B, u8
+template<> void Executor::Execute<0x06>(CpuState &state, Bus &bus)
 {
     state.BC.high = bus.Read(state.PC.reg++);
     state.t_cycles += 8;
 }
-template<> void Executor::Execute<7>(CpuState &state, Bus &bus)
+
+// RLCA
+template<> void Executor::Execute<0x07>(CpuState &state, Bus &bus)
 {
     SetZ(state, false);
     SetN(state, false);
@@ -81,4 +97,91 @@ template<> void Executor::Execute<7>(CpuState &state, Bus &bus)
     state.AF.high = ((state.AF.high & ~(1U << 0U)) | ((state.AF.low & (1U << 4U)) >> 4U));
 
     state.t_cycles += 4;
+}
+
+// LD (u16), SP
+template <> void Executor::Execute<0x08>(CpuState &state, Bus &bus)
+{
+  uint8_t lsb = bus.Read(state.PC.reg++);
+  uint8_t msb = bus.Read(state.PC.reg++);
+  uint16_t nn = ToU16(msb, lsb);
+  bus.Write(nn, state.SP.low);
+  ++nn;
+  bus.Write(nn, state.SP.high);
+  state.t_cycles += 20;
+}
+
+// ADD HL, BC
+template <> void Executor::Execute<0x09>(CpuState &state, Bus &bus)
+{
+  uint32_t res = static_cast<uint32_t>(state.HL.reg) + static_cast<uint32_t>(state.BC.reg);
+
+  SetN(state, false);
+  SetH(state, ((state.HL.reg & 0xFFFU) + (state.BC.reg & 0xFFFU)) > 0xFFFU);
+  SetCY(state, res >= 0xFFFFU);
+
+  state.HL.reg = res & 0xFFFFU;
+  state.t_cycles += 8;
+}
+
+// LD A, (BC)
+template <> void Executor::Execute<0x0A>(CpuState &state, Bus &bus)
+{
+  state.AF.high = bus.Read(state.BC.reg);
+  state.t_cycles += 8;
+}
+
+// DEC BC
+template <> void Executor::Execute<0x0B>(CpuState &state, Bus &bus)
+{
+  state.BC.reg = state.BC.reg - 1;
+  state.t_cycles += 8;
+}
+
+// INC C
+template <> void Executor::Execute<0x0C>(CpuState &state, Bus &bus)
+{
+  uint16_t res = static_cast<uint16_t>(state.BC.low) + 1;
+
+  SetZ(state, (res & 0xFFU) == 0);
+  SetN(state, false);
+  SetH(state, ((state.BC.low & 0xFU) + 1) > 0xFU);
+
+  state.BC.low = (res & 0xFFU);
+  state.t_cycles += 4;
+}
+
+// DEC C
+template <> void Executor::Execute<0x0D>(CpuState &state, Bus &bus)
+{
+  uint8_t res = state.BC.low - 1;
+
+  SetZ(state, res == 0);
+  SetN(state, true);
+  SetH(state, ((state.BC.low & 0xFU) - 1) > 0xFU);
+
+  state.BC.low = res;
+  state.t_cycles += 4;
+}
+
+// LD C, u8
+template <> void Executor::Execute<0x0E>(CpuState &state, Bus &bus)
+{
+  state.BC.low = bus.Read(state.PC.reg++);
+  state.t_cycles += 8;
+}
+
+// RRCA
+template <> void Executor::Execute<0x0F>(CpuState &state, Bus &bus)
+{
+  uint8_t b0 = (state.AF.high & (1U << 0U));
+
+  SetZ(state, false);
+  SetN(state, false);
+  SetH(state, false);
+  SetCY(state, b0);
+
+  state.AF.high = state.AF.high >> 1U;
+  state.AF.high = (state.AF.high & ~(1U << 7U)) | static_cast<uint8_t>(b0 << 7U);
+  state.t_cycles += 4;
 }
