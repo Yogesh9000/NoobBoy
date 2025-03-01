7df4f2d3d2e3a591e0371c4a811f8bb7b74a0899
Yogesh Rathod <yogesh952003@gmail.com>
2025-03-01 20:08:35 +0530

    Update Cpu::SetInitialState method
    update the SetInitialState method to also initialize the value of ime,
    IsHalted, ie_delay, t_cycles inside cpu state

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -47,4 +47,8 @@ void Cpu::SetInitialState()
   m_state.HL.reg = 0x014D;
   m_state.SP.reg = 0xFFFE;
   m_state.PC.reg = 0x0100;
+  m_state.ime = false;
+  m_state.Ishalted = false;
+  m_state.ie_delay = 0;
+  m_state.t_cycles = 0;
 }
